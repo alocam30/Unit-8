301339f1acc3bf517506dbf8b5b22faa4a6882c0
alocam30 <99086842+alocam30@users.noreply.github.com>
2022-11-08 12:32:41 -0800

    fixed homepage route

--- app.js
+++ app.js
@@ -27,6 +27,9 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+const routes = require('./routes/index');
+const users = require('./routes/users');
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -37,8 +40,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use('/static', express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', routes);
+app.use('/books', users);
 
 //  error handler
 app.use((req, res, next) => {
--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,44 @@
 var express = require('express');
 var router = express.Router();
-const Book = require('../models/book.js');
+const Book = require('../models').Book;
+const books = require('../models/book');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  (async () => {
+
+function asyncHandler(cb){
+  return async(req, res, next) => {
     try {
+      await cb(req, res, next)
+    } catch(error){
+      // res.status(500).send(error);
+      next(error);
+    }
+  }
+}
+
+    router.get('/', asyncHandler(async (req, res) => {
       const allBooks = await Book.findAll();
-      console.log( allBooks.map(res => res.json()) );
-    } catch (error) {
-    }}
-  )}
-  );
+      res.render("index", { books })
+    }))
+
+
+
+//shows full list of books
+
+//shows the create new book form
+
+//posts a new book to the database
+router.post
+
+//shows book detail form
+router.get("/books/:id", );
+
+
+//updates book info in the database
+router.post("/books/:id")
+
+//Deletes a book
+
+
 
 module.exports = router;
